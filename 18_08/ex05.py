b47c3011534899cdaf8a627fe67c3705f03229c0
fmvini <viniciusmarrocos7@gmail.com>
2026-08-18 21:48:37 -0300

    ajustes

--- 18_08/ex05.py
+++ 18_08/ex05.py
@@ -1,9 +1,9 @@
 min = int(input("Digite a faixa inferior: "))
 max = int(input("Digite a faixa superior: "))
-pares = 0
+pares = []
 
 for i in range(min, max + 1):
     if i % 2 == 0:
-        pares += 1
-    print(i, end=", ")
-print("\nA quantidade de pares entre " + str(min) + " e " + str(max) + " é: " + str(pares))
+        pares.append(i)
+    print(i, end=" ")
+print("\nA quantidade de pares entre " + str(min) + " e " + str(max) + " é: " + str(len(pares)))
--- 18_08/ex06.py
+++ 18_08/ex06.py
@@ -1,12 +1,12 @@
 min = int(input("Digite a faixa inferior: "))
 max = int(input("Digite a faixa superior: "))
 temp_max = max
-pares = 0
+pares = []
 
 for i in range(min, max + 1):
     if i % 2 == 0:
-        pares += 1
+        pares.append(i)
 for i in range(min, max + 1):
-    print(temp_max)
+    print(temp_max, end = " ")
     temp_max -= 1
-print("A quantidade de pares entre " + str(min) + " e " + str(max) + " é: " + str(pares))
+print("\nA quantidade de pares entre " + str(min) + " e " + str(max) + " é: " + str(len(pares)))
