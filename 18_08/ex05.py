a003242ef32b2aca8f05f17b96c2cbe0515eba6e
fmvini <viniciusmarrocos7@gmail.com>
2026-08-18 21:37:55 -0300

    pequenas correções

--- 18_08/ex05.py
+++ 18_08/ex05.py
@@ -5,5 +5,5 @@ pares = 0
 for i in range(min, max + 1):
     if i % 2 == 0:
         pares += 1
-    print(i)
-print("A quantidade de pares entre " + str(min) + " e " + str(max) + " é: " + str(pares))
+    print(i, end=", ")
+print("\nA quantidade de pares entre " + str(min) + " e " + str(max) + " é: " + str(pares))
